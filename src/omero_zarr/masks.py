64bf54f0c356330cfefd6cc525b6dcc315531bd4
Simon Li <spli@dundee.ac.uk>
2020-06-24 15:58:35 +0100

    Don't expand masks to all planes if C/Z/T is None

--- src/omero_zarr/masks.py
+++ src/omero_zarr/masks.py
@@ -5,6 +5,16 @@ import numpy as np
 import zarr
 
 
+# Mapping of dimension names to axes in the Zarr
+DIMENSION_ORDER = {
+    "T": 0,
+    "C": 1,
+    "Z": 2,
+    "Y": 3,
+    "X": 4,
+}
+
+
 def image_masks_to_zarr(image, args):
 
     size_t = image.getSizeT()
@@ -31,6 +41,23 @@ def image_masks_to_zarr(image, args):
     print(f"Found {len(masks)} masks")
 
     if masks:
+        # Figure out whether we can flatten some dimensions
+        unique_dims = {
+            "T": set(),
+            "C": set(),
+            "Z": set(),
+        }
+        for shapes in masks.values():
+            for mask in shapes:
+                unique_dims["T"].add(unwrap(mask.theT))
+                unique_dims["C"].add(unwrap(mask.theC))
+                unique_dims["Z"].add(unwrap(mask.theZ))
+        ignored_dimensions = set()
+        print(unique_dims)
+        for d in "TCZ":
+            if unique_dims[d] == {None}:
+                ignored_dimensions.add(d)
+
         name = f"{image.id}.zarr"
         root = zarr.open(name)
         if "masks" in root.group_keys():
@@ -38,16 +65,28 @@ def image_masks_to_zarr(image, args):
         else:
             out_masks = root.create_group("masks")
 
+        mask_shape = list(image_shape)
+        for d in ignored_dimensions:
+            mask_shape[DIMENSION_ORDER[d]] = 1
+        print("Ignoring dimensions {}".format(ignored_dimensions))
+
         # TODO: Make each ROI a separate group and use Roi.id as name?
         roi_name = "0"
         za = out_masks.create_dataset(
             roi_name,
-            shape=image_shape,
-            chunks=(1, 1, size_z, size_y, size_x),
+            shape=mask_shape,
+            chunks=(1, 1, 1, size_y, size_x),
             dtype=np.int16,
             overwrite=True,
         )
-        masks_to_labels(masks, image_shape, check_overlaps=False, labels=za)
+        masks_to_labels(
+            masks,
+            mask_shape,
+            ignored_dimensions,
+            check_overlaps=True,
+            labels=za,
+        )
+
         # Setting za.attrs[] doesn't work, so go via parent
         if "0" in root:
             image_name = "0"
@@ -69,26 +108,18 @@ def image_masks_to_zarr(image, args):
         print("No masks found on Image")
 
 
-def mask_to_binim(mask, image_shape):
+def _mask_to_binim_yx(mask):
     """
     :param mask MaskI: An OMERO mask
-    :param image_shape 5-tuple: the image dimensions (T, C, Z, Y, X)
 
-    :return: Binary mask with the same dimensions as the image
-             If `T`, `C` or `Z` are not set on the mask the mask is expanded to
-             all planes in that dimension
+    :return: tuple of
+            - Binary mask
+            - (T, C, Z, Y, X, w, h) tuple of mask settings (T, C, Z may be
+              None)
 
     TODO: Move to https://github.com/ome/omero-rois/
     """
-    size_t, size_c, size_z, size_y, size_x = image_shape
 
-    def get_indicies(d, size_d):
-        if d is not None:
-            return [d]
-        return range(size_d)
-
-    # Create an nd-array same size as image
-    binim = np.zeros(image_shape, np.bool)
     t = unwrap(mask.theT)
     c = unwrap(mask.theC)
     z = unwrap(mask.theZ)
@@ -105,51 +136,115 @@ def mask_to_binim(mask, image_shape):
     # truncate and reshape
     binarray = np.reshape(binarray[: (w * h)], (h, w))
 
-    for i_t in get_indicies(t, size_t):
-        for i_c in get_indicies(c, size_c):
-            for i_z in get_indicies(z, size_z):
-                binim[i_t, i_c, i_z, y : (y + h), x : (x + w)] = binarray
+    return binarray, (t, c, z, y, x, h, w)
+
+
+def _get_indicies(ignored_dimensions, d, d_value, d_size):
+    """
+    Figures out which Z/C/T-planes a mask should be copied to
+    """
+    if d in ignored_dimensions:
+        return [0]
+    if d_value is not None:
+        return [d_value]
+    return range(d_size)
+
+
+def mask_to_binim(mask, mask_shape, ignored_dimensions):
+    """
+    :param mask MaskI: An OMERO mask
+    :param mask_shape 5-tuple: the image dimensions (T, C, Z, Y, X), taking
+           into account `ignored_dimensions`
+    :param ignored_dimensions set(char): Ignore these dimensions and set size
+           to 1
+
+    :return: Binary mask with the same dimensions as the image
+             If `T`, `C` or `Z` are not set on the mask the mask is expanded to
+             all planes in that dimension
+
+    TODO: Move to https://github.com/ome/omero-rois/
+    """
+    size_t, size_c, size_z, size_y, size_x = mask_shape
+
+    binim = np.zeros(mask_shape, np.bool)
+    binim_yx, (t, c, z, y, x, h, w) = _mask_to_binim_yx(mask)
+
+    for i_t in _get_indicies(ignored_dimensions, "T", t, size_t):
+        for i_c in _get_indicies(ignored_dimensions, "C", c, size_c):
+            for i_z in _get_indicies(ignored_dimensions, "Z", z, size_z):
+                binim[i_t, i_c, i_z, y : (y + h), x : (x + w)] = binim_yx
 
     return binim
 
 
-def masks_to_labels(masks, image_shape, check_overlaps, labels=None):
+def masks_to_labels(
+    masks,
+    mask_shape,
+    ignored_dimensions=None,
+    check_overlaps=True,
+    labels=None,
+):
     """
     :param masks [MaskI]: Iterable container of OMERO masks
-    :param image_shape 5-tuple: the image dimensions (T, C, Z, Y, X)
+    :param mask_shape 5-tuple: the image dimensions (T, C, Z, Y, X), taking
+           into account `ignored_dimensions`
+    :param ignored_dimensions set(char): Ignore these dimensions and set size
+           to 1
     :param check_overlaps bool: Whether to check for overlapping masks or not
     :param labels nd-array: The optional output array, pass this if you have
            already created the array and want to fill it.
 
-    :return: Label image with the same dimensions as the image
-             If `T`, `C` or `Z` are not set on the mask the labels are expanded
-             to all planes in that dimension
+    :return: Label image with size `mask_shape`
 
     TODO: Move to https://github.com/ome/omero-rois/
     """
 
-    # Create np nd-array same size as image
+    size_t, size_c, size_z, size_y, size_x = mask_shape
+    ignored_dimensions = ignored_dimensions or set()
+    mask_shape = tuple(mask_shape)
+
     if not labels:
         # TODO: Set np.int size based on number of labels
-        labels = np.zeros(image_shape, np.int16)
-    assert (
-        labels.shape == image_shape
-    ), "labels must have the same shape as the image"
+        labels = np.zeros(mask_shape, np.int16)
+
+    for d in "TCZYX":
+        if d in ignored_dimensions:
+            assert (
+                labels.shape[DIMENSION_ORDER[d]] == 1
+            ), "Ignored dimension {} should be size 1".format(d)
+        assert (
+            labels.shape == mask_shape
+        ), "Invalid label shape: {}, expected {}".format(
+            labels.shape, mask_shape
+        )
 
     for count, shapes in enumerate(masks.values()):
         # All shapes same color for each ROI
         print(count)
         for mask in shapes:
-            binim = mask_to_binim(mask, image_shape)
-            if check_overlaps and np.any(
-                np.logical_and(labels.astype(np.bool), binim)
-            ):
-                raise Exception(
-                    "Mask {} overlaps with existing labels".format(count)
-                )
-
-            # ADD to the array, so zeros in our binarray don't wipe out
-            # previous masks
-            labels[:] += binim * count
+            binim_yx, (t, c, z, y, x, h, w) = _mask_to_binim_yx(mask)
+            for i_t in _get_indicies(ignored_dimensions, "T", t, size_t):
+                for i_c in _get_indicies(ignored_dimensions, "C", c, size_c):
+                    for i_z in _get_indicies(
+                        ignored_dimensions, "Z", z, size_z
+                    ):
+                        if check_overlaps and np.any(
+                            np.logical_and(
+                                labels[
+                                    i_t, i_c, i_z, y : (y + h), x : (x + w)
+                                ].astype(np.bool),
+                                binim_yx,
+                            )
+                        ):
+                            raise Exception(
+                                "Mask {} overlaps with existing labels".format(
+                                    count
+                                )
+                            )
+                        # ADD to the array, so zeros in our binarray don't
+                        # wipe out previous masks
+                        labels[i_t, i_c, i_z, y : (y + h), x : (x + w)] += (
+                            binim_yx * count
+                        )
 
     return labels
